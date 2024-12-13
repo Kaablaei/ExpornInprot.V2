167634249f43c8a259dda399bf7549b0cdd2c102
AmirHossein-Biabani <amirhosseinbiabany@yahoo.com>
2024-12-14 01:25:21 +0330

    تاحدودی درست کار میکند

--- ExportInportWPF/Menu/hesabdari/AsnadeHeabdari_UserContoller.xaml.cs
+++ ExportInportWPF/Menu/hesabdari/AsnadeHeabdari_UserContoller.xaml.cs
@@ -29,10 +29,10 @@ namespace ExportInportWPF.Menu.hesabdari
         public AsnadeHeabdari_UserContoller(ICodingKollRepository ripo)
         {
             InitializeComponent();
-      
 
 
-            
+
+
             Accounts = new ObservableCollection<Account>();
             Koll_DataGrid.ItemsSource = Accounts;
             _ripo = ripo;
@@ -41,48 +41,94 @@ namespace ExportInportWPF.Menu.hesabdari
 
         private void Koll_DataGrid_PreparingCellForEdit(object sender, DataGridPreparingCellForEditEventArgs e)
         {
-        
-            if (e.Column.DisplayIndex == 0) 
+
+            if (e.Column.DisplayIndex == 0)
             {
 
-                Koll_listBox.ItemsSource = _ripo.GetAll();
-                Koll_Pupop.IsOpen = true;
+                Koll_ListBox.ItemsSource = _ripo.GetAll();
+                Koll_Popup.IsOpen = true;
             }
 
-      
+
 
         }
 
+
+        // تعریف لیست اصلی
+        private List<CodeingKoll> _originalItems;
+
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                // موقعیت فعلی TextBox
+                var point = textBox.GetRectFromCharacterIndex(textBox.CaretIndex, true).BottomLeft;
+
+                // تبدیل به مختصات کلی پنجره
+                var transform = textBox.TransformToVisual(this);
+                var popupPosition = transform.Transform(point);
+
+                // تنظیم موقعیت Popup
+                Koll_Popup.HorizontalOffset = popupPosition.X;
+                Koll_Popup.VerticalOffset = popupPosition.Y;
+
+                // باز کردن Popup
+                Koll_Popup.IsOpen = true;
+
+                // فیلتر کردن لیست
+                string filterText = textBox.Text.ToLower();
+                if (!string.IsNullOrWhiteSpace(filterText) && _originalItems != null)
+                {
+                    Koll_ListBox.ItemsSource = _originalItems
+                        .Where(item => item.CodeKoll.ToString().StartsWith(filterText))
+                        .ToList();
+                }
+            }
+        }
+
+
+        private void Koll_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Koll_ListBox.SelectedItem != null)
+            {
+                var selectedItem = Koll_ListBox.SelectedItem;
+
+                // گرفتن آیتم انتخاب‌شده در DataGrid
+                var currentRow = Koll_DataGrid.SelectedItem as Account;
+                if (currentRow != null)
+                {
+                    // مقداردهی کد کل
+                    currentRow.KollCode = selectedItem.GetType().GetProperty("CodeKoll")?.GetValue(selectedItem)?.ToString();
+                }
+
+                // بستن Popup
+                Koll_Popup.IsOpen = false;
+                Koll_ListBox.SelectedItem = null;
+                FilterTextBox.Text = string.Empty;
+
+                // بازگرداندن فوکوس به سلول فعلی DataGrid
+                Koll_DataGrid.BeginEdit();
+            }
+        }
 
 
 
         public ObservableCollection<Account> Accounts { get; set; }
 
 
-        private void Koll_listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+
+
+        public class Account
         {
-            if (Koll_listBox.SelectedItem != null)
-            {
-                var selectedItem = Koll_listBox.SelectedItem as Account;
-                var dataGridRow = Koll_DataGrid.SelectedItem as Account;
-                dataGridRow.KollCode = selectedItem.KollCode;  // انتقال کد کل به سلول
-                Koll_Pupop.IsOpen = false;  // بستن پاپ‌آپ
-                Koll_DataGrid.SelectedIndex += 1;  // حرکت به ستون بعدی
-            }
+            public string KollCode { get; set; }
+            public string MoeinCode { get; set; }
+            public string Tafsili { get; set; }
+            public string Description { get; set; }
+            public decimal Credit { get; set; }
+            public decimal Debit { get; set; }
         }
-
-     
-    }
-
-
-    public class Account
-    {
-        public string KollCode { get; set; }
-        public string MoeinCode { get; set; }
-        public string Tafsili { get; set; }
-        public string Description { get; set; }
-        public decimal Credit { get; set; }
-        public decimal Debit { get; set; }
     }
 }
 
