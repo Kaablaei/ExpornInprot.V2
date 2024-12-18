8d6e6898d6af236b7dbe169015e0e9d1370b4dae
AmirHossein-Biabani <amirhosseinbiabany@yahoo.com>
2024-12-18 22:21:34 +0330

    Adding Combobox to hesab

--- ExportInportWPF/Menu/hesabdari/AsnadeHeabdari_UserContoller.xaml.cs
+++ ExportInportWPF/Menu/hesabdari/AsnadeHeabdari_UserContoller.xaml.cs
@@ -26,108 +26,38 @@ namespace ExportInportWPF.Menu.hesabdari
     {
 
         private ICodingKollRepository _ripo;
+        public ObservableCollection<RowData> FixedRows { get; set; }
         public AsnadeHeabdari_UserContoller(ICodingKollRepository ripo)
         {
             InitializeComponent();
 
-
-
-
-            Accounts = new ObservableCollection<Account>();
-            Koll_DataGrid.ItemsSource = Accounts;
             _ripo = ripo;
 
-        }
-
-        private void Koll_DataGrid_PreparingCellForEdit(object sender, DataGridPreparingCellForEditEventArgs e)
-        {
-
-            if (e.Column.DisplayIndex == 0)
+            FixedRows = new ObservableCollection<RowData>();
+            for (int i = 0; i < 30; i++)
             {
-                Koll_ListBox.ItemsSource = _ripo.GetAll();
-                Koll_Popup.IsOpen = true;
+                FixedRows.Add(new RowData { RowNumber = i + 1 }); // مقداردهی شماره ردیف
             }
 
-
+            DataContext = this;
 
         }
 
 
-        // تعریف لیست اصلی
-        private List<CodeingKoll> _originalItems;
 
 
-        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+
+        public class RowData
         {
-            if (sender is TextBox textBox)
-            {
-                // موقعیت فعلی TextBox
-                var point = textBox.GetRectFromCharacterIndex(textBox.CaretIndex, true).BottomLeft;
-
-                // تبدیل به مختصات کلی پنجره
-                var transform = textBox.TransformToVisual(this);
-                var popupPosition = transform.Transform(point);
-
-                // تنظیم موقعیت Popup
-                Koll_Popup.HorizontalOffset = popupPosition.X;
-                Koll_Popup.VerticalOffset = popupPosition.Y;
-
-                // باز کردن Popup
-                Koll_Popup.IsOpen = true;
-
-                // فیلتر کردن لیست
-                string filterText = textBox.Text.ToLower();
-                if (!string.IsNullOrWhiteSpace(filterText) && _originalItems != null)
-                {
-                    Koll_ListBox.ItemsSource = _originalItems
-                        .Where(item => item.CodeKoll.ToString().StartsWith(filterText))
-                        .ToList();
-                }
-            }
-        }
-
-
-        private void Koll_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (Koll_ListBox.SelectedItem != null)
-            {
-                var selectedItem = Koll_ListBox.SelectedItem;
-
-                // گرفتن آیتم انتخاب‌شده در DataGrid
-                var currentRow = Koll_DataGrid.SelectedItem as Account;
-                if (currentRow != null)
-                {
-                    // مقداردهی کد کل
-                    currentRow.KollCode = selectedItem.GetType().GetProperty("CodeKoll")?.GetValue(selectedItem)?.ToString();
-                }
-
-                // بستن Popup
-                Koll_Popup.IsOpen = false;
-                Koll_ListBox.SelectedItem = null;
-                FilterTextBox.Text = string.Empty;
-
-                // بازگرداندن فوکوس به سلول فعلی DataGrid
-                Koll_DataGrid.BeginEdit();
-            }
-        }
-
-
-
-        public ObservableCollection<Account> Accounts { get; set; }
-
-
-
-
-
-        public class Account
-        {
+            public int RowNumber { get; set; } // شماره ردیف
             public string KollCode { get; set; }
             public string MoeinCode { get; set; }
             public string Tafsili { get; set; }
             public string Description { get; set; }
-            public decimal Credit { get; set; }
             public decimal Debit { get; set; }
+            public decimal Credit { get; set; }
         }
+
     }
 }
 
